4272c7e1f0a6793518ad4007029407b49960a5f4
dqthien <64117502+dqthien@users.noreply.github.com>
2023-08-21 10:35:06 +0700

    Update phần query data của Trung

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -14,18 +14,37 @@ class LeaderboardController extends Controller
 {
     public function index()
     {
-        // Fetch users' data from an API or database
-         //$jsonPath = public_path('leaderboard.json');
-         //$jsonData = File::get($jsonPath);
+        // // Fetch users' data from an API or database
+        //  //$jsonPath = public_path('leaderboard.json');
+        //  //$jsonData = File::get($jsonPath);
         
-         //$users = json_decode($jsonData, true);  
-        $users=Leaderboard::all();
-        $users=$users->toArray();
-        // Sort users by contributions (post IDs) in descending order
-        usort($users, function ($a, $b) {
-            return $b['contributions'] - $a['contributions'];
-        });
-        //$users = $users->sortByDesc('contributions');
+        //  //$users = json_decode($jsonData, true);  
+        // $users=Leaderboard::all();
+        // $users=$users->toArray();
+        // // Sort users by contributions (post IDs) in descending order
+        // usort($users, function ($a, $b) {
+        //     return $b['contributions'] - $a['contributions'];
+        // });
+        // //$users = $users->sortByDesc('contributions');
+        $users = DB::connection('mongodb')->collection('Account')
+        ->orderBy('Follower', 'desc')
+        ->limit(100)
+        ->get(); //get data as array
+
+        // query data using for loop and i
+        // for($i = 0; $i < 100; $i++){
+        //     echo $users[$i]['Nickname'];
+        //     echo $users[$i]['Follower'];
+        //     echo "<br>";
+        // }
+
+        //query data using foreach
+        // foreach ($users as $user){
+        //     echo $user['Nickname'];
+        //     echo $user['Follower'];
+        //     echo "<br>";
+        // }
+
         return view('leaderboard.index', ['users' => $users]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\Homecontroller;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\LeaderboardController;
 
 /*
 |--------------------------------------------------------------------------
